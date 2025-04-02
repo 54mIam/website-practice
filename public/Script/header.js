7e77d28b7fe0448bd7c49d55aadc65bbea1639da
sir-kayos <sammieb38@gmail.com>
2025-04-02 04:18:15 -0400

    added links to footer and changed more styling

--- public/Script/header.js
+++ public/Script/header.js
@@ -5,3 +5,11 @@ menu.addEventListener("click", function() {
     menu.classList.toggle("is-active");
     menuLinks.classList.toggle("active");
 });
+
+document.querySelector(".index-btn").addEventListener("click", function () {
+    document.querySelector(".page-info").classList.add("hide");
+
+    // Show the webpage-container section
+    document.querySelector(".webpage-container").classList.remove("hide");
+
+});
